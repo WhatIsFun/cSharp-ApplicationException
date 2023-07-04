8cef04338b73f24dba6009f1ac6fd077be230f7e
WhatIsFun <mohammed110051@gmail.com>
2023-07-04 11:20:24 +0400

    Application Exception

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace cSharp_ApplicationException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students =
+            {
+                new Student("Adhary","1233", 19),
+                new Student("Mohd", "1234", 24),
+                new Student("Ahmed","1233", 50)
+
+            };
+            Console.WriteLine(Student.validateStudents(students));
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_ApplicationException
+{
+    internal class Student
+    {
+        String name { get; set; }
+        String regstrationNumber { get; set; }
+        int age { get; set; }
+
+        public Student(string name, string regstrationNumber, int age)
+        {
+            this.name = name;
+            this.regstrationNumber = regstrationNumber;
+            this.age = age;
+        }
+
+        public static Boolean validateStudents(Student[] students) {
+            for (int i = 0; i < students.Length; i++)
+            {
+                for (int j = 0 +1; j < students.Length; j++)
+                {
+                    if (students[i].regstrationNumber == students[j].regstrationNumber)
+                    {
+                        //return false;
+                        throw new ApplicationException("Two student have same regstration number.");
+
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
